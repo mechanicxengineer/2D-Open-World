290d80b37bbad3a2a83f835af66e3230024020f9
mechanicxengineer <monumanish500@gmail.com>
2025-09-25 00:53:53 +0530

    Player and Tile maps
    - Created player and player movement
    - Setup tile palettes for making levels

---
+++ Assets/SourceCode/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed;
+	private Rigidbody2D rigidbody2d;
+	private Vector3 change;
+	private Animator animator;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rigidbody2d = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		change = Vector3.zero;
+		change.x = Input.GetAxisRaw("Horizontal");
+		change.y = Input.GetAxisRaw("Vertical");
+		Debug.Log(change);
+		UpdateAnimationAndMove();
+	}
+
+	void UpdateAnimationAndMove()
+	{
+		if (change != Vector3.zero)
+		{
+			MoveCharacter();
+			animator.SetFloat("moveX", change.x);
+			animator.SetFloat("moveY", change.y);
+			animator.SetBool("moving", true);
+		}
+		else { animator.SetBool("moving", false); }
+	}
+
+	void MoveCharacter()
+	{
+		rigidbody2d.MovePosition(transform.position + change * speed * Time.deltaTime);
+	}
+}
